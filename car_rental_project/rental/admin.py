11025955f638c7c7a0131d3939821743a9d7e11b
Tedi <tedimihali1@gmail.com>
2023-12-02 17:38:24 +0100

    Fixed car model and added str functions:

--- car_rental_project/rental/admin.py
+++ car_rental_project/rental/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Car, Booking, Customer
+from .models import Car, Booking, Customer, CarImage
 
+class CarImageInline(admin.TabularInline):
+    model = CarImage
+    extra = 1  # Number of empty forms to show for adding new images
 
-# Register your models here.
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['brand', 'model', 'production_year','fuel', 'gearbox', 'color', 'daily_rate']
+    list_display = ['brand', 'model', 'production_year', 'fuel', 'gearbox', 'color', 'daily_rate']
     search_fields = ['brand', 'model']
+    inlines = [CarImageInline]  # Include CarImageInline in the CarAdmin form
+
+    
+
 
 admin.site.register(Car, CarAdmin)
-admin.site.register(Booking);
-admin.site.register(Customer);
+admin.site.register(Booking)
+admin.site.register(Customer)
+admin.site.register(CarImage)
