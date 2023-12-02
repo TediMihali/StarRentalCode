a76a895629a53eeb89b30e0d1f5dadb3e1eb7b8e
Tedi <tedimihali1@gmail.com>
2023-12-02 22:43:30 +0100

    Started on the main page functionality

--- car_rental_project/car_rental_project/urls.py
+++ car_rental_project/car_rental_project/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rentals/", include("rental.urls"))
+    path("", include("rental.urls"))
 ]
 
 if settings.DEBUG:
