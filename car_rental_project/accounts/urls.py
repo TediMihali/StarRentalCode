d1ac93818e8543352c0b26240c08e23da9495d11
Tedi <tedimihali1@gmail.com>
2023-12-03 16:04:04 +0100

    Added login and logout functionality as well as more styling

--- car_rental_project/accounts/urls.py
+++ car_rental_project/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignInView, SignUpView
+from .views import SignInView, SignUpView, logout_view
 urlpatterns = [
     path("/signin/", SignInView.as_view(), name='signin'),
-    path("/signup/", SignUpView.as_view(), name="signup")
+    path("/signup/", SignUpView.as_view(), name="signup"),
+    path("/logout/", logout_view, name="logout")
 ]
