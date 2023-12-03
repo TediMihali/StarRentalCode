8da464877ca3fd97e6ca1fb4f5fbee4cf793c730
Tedi <tedimihali1@gmail.com>
2023-12-03 10:46:08 +0100

    Worked on the UI and started the authentication

---
+++ car_rental_project/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",'id', 'password1', "password2")
+
+class SignInForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password")
---
+++ car_rental_project/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path("/signup/", , name='signup'),
+    path("/signin/", , name="signin")
+]
